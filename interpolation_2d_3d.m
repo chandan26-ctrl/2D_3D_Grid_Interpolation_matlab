842c4875646538e7b11f148a43fc260839a28baf
ChandanKr <76987249+chandan26-ctrl@users.noreply.github.com>
2025-01-21 13:12:13 -0500

    Add files via upload

---
+++ interpolation_2d_3d.m
@@ -0,0 +1,39 @@
+%2D to New 2D grid
+
+% old grid x,y and variable um to be interpolated on 
+% new grid xq(il,jl), yq as uq
+
+xnn = (reshape(x,[],1));
+ynn = (reshape(y,[],1));
+dum = (reshape(um,[],1));
+F = scatteredInterpolant(xnn,ynn,dum);
+
+dxn = (reshape(xq,[],1));
+dyn = (reshape(yq,[],1));
+
+
+   dm1 = F(dxn,dyn);
+   dm2 = reshape(dm1,[il,jl]);
+   uq(:,:,:) = dm2(:,:);
+
+
+
+%3D to New 3D grid
+
+% old grid x,y,z and variable um to be interpolated on 
+% new grid xq(il,jl,kl), yq, zq as uq
+xnn = (reshape(x,[],1,1));
+ynn = (reshape(y,[],1,1));
+znn = (reshape(z,[],1,1));
+dum = (reshape(um,[],1,1));
+F = scatteredInterpolant(xnn,ynn,znn,dum);
+
+dxn = (reshape(xq,[],1,1));
+dyn = (reshape(yq,[],1,1));
+dzn = (reshape(zq,[],1,1));
+
+ dm1 = F(dxn,dyn,dzn);
+ dm2 = reshape(dm1,[il,jl,kl]);
+ uq(:,:,:) = dm2(:,:,:);
+
+ 
